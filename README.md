b9ff4b9871bc6af6e279496ec0aed7d8e2dc8513
Mr.Mario <Mr.Mario@users.noreply.github.com>
2020-08-05 16:44:55 +0100

    update

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Real_Time_Car_Recognication_Based_On_Bag_of_Visual_Words
  
 ---
+In verification, we have 126 images, 63 for BMW, 63 for others.
+
 | |SIFT|SURF|Time Cost(sec)|
 |:-:|:-:|:-:|:-:|
-|Manhattan  |![](./images/Model_Evaluation/SIFT_Manhattan.png)    |![](./images/Model_Evaluation/SURF_Manhattan.png)  |0.698, 1.612|
-|Euclidean  |![](./images/Model_Evaluation/SIFT_Euclidean.png)    |![](./images/Model_Evaluation/SURF_Euclidean.png)  |0.923, 2.259|
-|Chebyshev  |![](./images/Model_Evaluation/SIFT_Chebyshev.png)    |![](./images/Model_Evaluation/SURF_Chebyshev.png)  |0.727, 1.686|
-|Cosine     |![](./images/Model_Evaluation/SIFT_Cosine.png)       |![](./images/Model_Evaluation/SURF_Cosine.png)     |1.871, 5.245|
+|Manhattan  |![](./images/Model_Evaluation/SIFT_Manhattan.png)    |![](./images/Model_Evaluation/SURF_Manhattan.png)  |1.420, 2.548|
+|Euclidean  |![](./images/Model_Evaluation/SIFT_Euclidean.png)    |![](./images/Model_Evaluation/SURF_Euclidean.png)  |1.887, 3.467|
+|Chebyshev  |![](./images/Model_Evaluation/SIFT_Chebyshev.png)    |![](./images/Model_Evaluation/SURF_Chebyshev.png)  |1.487, 2.662|
+|Cosine     |![](./images/Model_Evaluation/SIFT_Cosine.png)       |![](./images/Model_Evaluation/SURF_Cosine.png)     |4.176, 7.968|
---
+++ TEST/test.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cv2
+import datetime
+from statistics import mode
+
+k = 1
+
+BoVW_Model = np.load("../result/Key_Points_Description_by_SIFT.npy", allow_pickle=True)
+dataset = np.load("../result/dataset/SIFT_Euclidean.npy", allow_pickle=True).item()
+
+SIFT = cv2.xfeatures2d.SIFT_create()     
+
+def SIFT_features(image):                                                                     
+    key_point, description = SIFT.detectAndCompute(image,None)                           
+    return key_point, description
+
+
+def find_index(each_feature, BoVW_Model):
+    count = 0
+    ind = 0
+    for i in range(len(BoVW_Model)):
+        if(i == 0):
+           count = np.sqrt(np.sum(np.square(each_feature - BoVW_Model[i]))) 
+        else:
+            dist = np.sqrt(np.sum(np.square(each_feature - BoVW_Model[i])))
+            if(dist < count):
+                ind = i
+                count = dist
+    return ind 
+        
+def build_dataset(description):                    
+    histogram = np.zeros(len(BoVW_Model))
+    for each_feature in description:
+        ind = find_index(each_feature, BoVW_Model)
+        histogram[ind] += 1
+    
+    return histogram
+
+
+image = cv2.imread("./Positive.jpg", cv2.IMREAD_GRAYSCALE) 
+'''
+cv2.imshow("Image", image)
+cv2.waitKey (0)  
+cv2.destroyAllWindows() 
+'''
+start_time = datetime.datetime.now()
+key_points, description = SIFT_features(image)
+
+histogram = build_dataset(description)
+
+buff = []
+for d_key, d_histograms in dataset.items():                        
+    for d_histogram in d_histograms:            
+        buff.append([np.sum(np.abs(histogram - d_histogram)), d_key])
+        
+list.sort(buff)        
+k_key = mode(np.asarray(buff[:k])[:, 1])
+end_time = datetime.datetime.now()
+print(end_time - start_time)
